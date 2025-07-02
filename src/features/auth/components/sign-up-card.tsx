1316e668449cec8b8da16ce8f941447ef6bbe15f
Christian <Christian.m.panerio@gmail.com>
2025-07-02 12:01:56 +0800

    03: database setup

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -61,7 +61,7 @@ export const SignUpCard = ({ setState }: Props) => {
                             </div>
 
                             <Button type="submit" className="w-full" size="lg" disabled={false}>
-                                Sign In
+                                Sign Up
                             </Button>
                             <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                                 <span className="bg-card text-muted-foreground relative z-10 px-2">
