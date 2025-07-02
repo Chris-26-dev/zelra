93c87306e9da599b42a734db812afdf178d40f69
Christian <Christian.m.panerio@gmail.com>
2025-07-02 17:17:40 +0800

    07: user button component

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -6,21 +6,55 @@ import { FaGoogle, FaGithub } from "react-icons/fa"
 import { SignInFlow } from "../types"
 import Image from "next/image"
 import { useState } from "react"
+import { OctagonAlertIcon } from "lucide-react"
+import { useAuthActions } from "@convex-dev/auth/react"
 
 interface Props {
     setState: (state: SignInFlow) => void;
 }
 
 export const SignUpCard = ({ setState }: Props) => {
+    const { signIn } = useAuthActions();
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const [pending, setPending] = useState(false);
+
+    const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+
+        setPending(true);
+        signIn("password", { name, email, password, flow: "signUp" })
+            .catch(() => {
+                setError("Something went wrong");
+            })
+            .finally(() => {
+                setPending(false);
+            });
+    };
+
+    const onProviderSignUp = (value: "github" | "google") => {
+        setPending(true)
+        signIn(value)
+            .finally(() => {
+                setPending(false);
+            })
+    };
+
+
 
     return (
         <div className="flex flex-col gap-6">
             <Card className="overflow-hidden p-0 border-0 shadow-none">
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <form className="p-6 md:p-8">
+                    <form onSubmit={onPasswordSignUp} className="p-6 md:p-8">
                         <div className="flex flex-col gap-6">
                             <div className="flex flex-col items-center text-center">
                                 <h1 className="text-2xl font-bold">
@@ -32,26 +66,30 @@ export const SignUpCard = ({ setState }: Props) => {
                             </div>
                             <div className="grid gap-3">
                                 <Input
+                                    disabled={pending}
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Full name"
+                                    required
+                                />
+                                <Input
+                                    disabled={pending}
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     placeholder="Email"
                                     type="email"
                                     required
                                 />
-                            </div>
-                            <div className="grid gap-3">
                                 <Input
-                                    disabled={false}
+                                    disabled={pending}
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
                                     placeholder="Password"
                                     type="password"
                                     required
                                 />
-                            </div>
-                            <div className="grid gap-3">
                                 <Input
-                                    disabled={false}
+                                    disabled={pending}
                                     value={confirmPassword}
                                     onChange={(e) => setConfirmPassword(e.target.value)}
                                     placeholder="Confirm password"
@@ -59,8 +97,13 @@ export const SignUpCard = ({ setState }: Props) => {
                                     required
                                 />
                             </div>
-
-                            <Button type="submit" className="w-full" size="lg" disabled={false}>
+                            {!!error && (
+                                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive">
+                                    <OctagonAlertIcon className="size-4" />
+                                    <p>{error}</p>
+                                </div>
+                            )}
+                            <Button type="submit" className="w-full" size="lg" disabled={pending}>
                                 Sign Up
                             </Button>
                             <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -70,6 +113,8 @@ export const SignUpCard = ({ setState }: Props) => {
                             </div>
                             <div className="grid grid-cols-2 gap-4">
                                 <Button
+                                    disabled={pending}
+                                    onClick={() => onProviderSignUp("google")}
                                     variant="outline"
                                     type="button"
                                     className="w-full"
@@ -77,6 +122,8 @@ export const SignUpCard = ({ setState }: Props) => {
                                     <FaGoogle />
                                 </Button>
                                 <Button
+                                    disabled={pending}
+                                    onClick={() => onProviderSignUp("github")}
                                     variant="outline"
                                     type="button"
                                     className="w-full"
