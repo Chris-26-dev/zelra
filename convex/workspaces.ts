fb497ef08bcf41505d31f7c4987c4e04c2f410b6
Christian <Christian.m.panerio@gmail.com>
2025-07-03 19:29:29 +0800

    11: workspace members

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,15 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+const generateCode = () => {
+    const code = Array.from(
+        { length: 6 },
+        ()  => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+    ).join("");
+
+    return code;
+}
+
 export const create = mutation({
     args: {
         name: v.string(),
@@ -13,8 +22,7 @@ export const create = mutation({
             throw new Error("Unauthorized");
         }
 
-        //TODO: Create a proper method later
-        const joinCode = "123456";
+        const joinCode = generateCode();
 
         const workspaceId = await ctx.db.insert("workspaces", {
             name: args.name,
@@ -22,14 +30,45 @@ export const create = mutation({
             joinCode,
         });
 
+        //Every user that creates a new workspace will also be a member of that workspace
+        await ctx.db.insert("members", {
+            userId,
+            workspaceId,
+            role: "admin"
+        });
+
         return workspaceId;
     },
 });
 
+//This get query should only show me workspaces i am a part of
 export const get = query({
     args: {},
     handler: async (ctx) => {
-        return await ctx.db.query("workspaces").collect();
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+        //Find the workspaces that this user is member
+        const members = await ctx.db
+            .query("members")
+            .withIndex("by_user_id", (q) => q.eq("userId", userId))
+            .collect()
+
+        const workspaceIds = members.map((member) => member.workspaceId);
+
+        const workspaces = [];
+
+        for (const workspaceId of workspaceIds) {
+            const workspace = await ctx.db.get(workspaceId);
+
+            if (workspace) {
+                workspaces.push(workspace);
+            }
+        }
+
+        return workspaces;
     },
 });
 
@@ -41,6 +80,17 @@ export const getById = query({
 
         if (!userId) {
             throw new Error("Unauthorized");
+        }
+
+        const member = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_user_id", (q) => 
+                q.eq("workspaceId", args.id).eq("userId", userId),
+            )
+            .unique();
+
+        if (!member) {
+            return null;
         }
 
         return await ctx.db.get(args.id);
