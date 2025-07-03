4b45bbc01f487dc4d638f3ae87b79352c16297c3
Christian <Christian.m.panerio@gmail.com>
2025-07-03 21:36:15 +0800

    12: workspace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface Props {
     children: React.ReactNode;
@@ -13,7 +16,23 @@ const WorkspaceLayout = ({ children }: Props) => {
             <Toolbar />
             <div className="flex h-[calc(100vh-40px)]">
                 <Sidebar />
-                {children}
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    autoSaveId="za-workspace-layout"
+                >
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={11}
+                        //sidebar color
+                        className="bg-blue-500"
+                    >
+                        <WorkspaceSidebar />
+                    </ResizablePanel>
+                    <ResizableHandle withHandle/>
+                    <ResizablePanel minSize={20}>
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
             </div>
         </div>
     );
